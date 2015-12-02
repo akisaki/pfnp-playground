0ed1467047d2e2f98b8746ed98d57fddf1ebd6bb
99:a9:89:fd:e2:cb:02:57:f9:1a:76:95:fc:28:be:e999:a9:89:fd:e2:cb:02:57:f9:1a:76:95:fc:28:be:e9akisaki <akisaki@mac.com>
2015-12-02 20:29:34 +0800

    button

---
+++ public/motd.js
@@ -0,0 +1,14 @@
+fetch('/motd').then(function(response){
+  response.text().ten(function(message){
+    document.querySelector('h1')
+    .textContent = message
+  })
+})
+
+document.querySelector('button')
+.addEventListener('click', function(){
+  var motd = window.prompt('Change the MOTD')
+  fetch('/edit/' + motd)
+document.querySelector('h1')
+.textContent = motd
+})
